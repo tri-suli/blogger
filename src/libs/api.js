e2ebc275cb1375390532aab5a959a05b69edce15
tri <trisulipras@gmail.com>
2023-11-05 23:16:19 +0700

    TBA-07: Create Base Api Service Using Axios
    - Initiate axios instance

---
+++ src/libs/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_SERVICE,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+});
+
+export default api;
