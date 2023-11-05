28dd23099d707cc2cc76faf68d4af50eb2fc4e73
tri <trisulipras@gmail.com>
2023-11-05 22:05:10 +0700

    Initial Project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { Container } from "reactstrap";
+
+function App() {
+  return (
+    <Container>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Blogger App
+          </p>
+        </header>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
